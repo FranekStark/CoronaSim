7f035305139fb80241534d9d1b8ce7b8bcbe9883
Franek Stark <franek@stark-berger.de>
2020-04-27 15:35:31 +0200

    add sttingsclass

---
+++ MedicalSettings.java
@@ -0,0 +1,12 @@
+/**
+ * This Class contains all Magic-Numbers which regards the medical topic
+ */
+public class MedicalSettings {
+
+    /**
+     * How many ticks is a infection long
+     */
+    public static int infection_duration = 14; //In ticks
+
+
+}
