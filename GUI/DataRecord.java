0bf09d32a1ea0f6b37a8c398e2e374aa44245a5b
Franek Stark <franek@stark-berger.de>
2020-05-20 01:07:03 +0200

    begin layout

---
+++ GUI/DataRecord.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import java.util.function.DoubleSupplier;
+
+import javafx.collections.ObservableList;
+
+public class DataRecord {
+
+    private String _name;
+    private DoubleSupplier _valueMethod;
+    private ObservableList<Double> _values;
+
+
+}
