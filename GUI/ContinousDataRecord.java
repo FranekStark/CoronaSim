46aef5f383055febe4db07dbf37a7b1dfdddacfa
Franek Stark <franek@stark-berger.de>
2020-05-24 17:56:00 +0200

    ..

---
+++ GUI/ContinousDataRecord.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+
+public class ContinousDataRecord<T> {
+
+    private String _name;
+    private Supplier<T> _valueMethod;
+    private ObservableLis
+    private boolean _visible;
+
+    public ContinousDataRecord(String name, Supplier<T> valueMehtod) {
+        
+    }
+
+}
